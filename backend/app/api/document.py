e5f46d46a6b2651a3c1c7661598d67084aa9cdb4
Samuel-Tefera <samuelteferab@gmail.com>
2026-02-12 09:32:28 +0300

    refactor: move document processing logic to utils

--- backend/app/api/document.py
+++ backend/app/api/document.py
@@ -10,38 +10,11 @@ from app.db.database import get_db
 from app.utils.utils import get_page_count
 from app.core.deps import get_current_user
 from app.repositories.document_repository import DocumentRepository
-from app.services.document_service import DocumentService
-from app.services.chunking_service import ChunkingService
-from app.repositories.chunk_repository import ChunkRepository
-from app.models.document import DocumentStatus, Document
 from app.schemas.document import DocumentOut
-from app.services.embedding_service import EmbeddingService
+from app.utils.utils import process_document_task
+
 
 router = APIRouter(prefix="/documents", tags=["document"])
-
-def process_document_task(db: Session, document_id: UUID, file_bytes: bytes, filename: str):
-    try:
-        # 1. EXTRACT TEXT
-        document_text = DocumentService.extract_text(file_bytes, filename)
-
-        # 2. GENERATE CHUNKS
-        document_chunks = ChunkingService.split_text(document_text)
-
-        # 3. SAVE CHUNKS TO DB
-        chunks = ChunkRepository.bulk_create_chunks(
-            db, document_id, document_chunks
-        )
-
-        EmbeddingService.create_embeddings(db, chunks)
-
-        DocumentRepository.update_document_status(
-            db, document_id, DocumentStatus.ready
-        )
-    except Exception as e:
-        print(f"Error processing document {document_id}: {e}")
-        DocumentRepository.update_document_status(
-            db, document_id, DocumentStatus.failed
-        )
 
 @router.post("/upload", response_model=DocumentOut)
 async def upload_document(
--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -2,6 +2,16 @@ import io
 from pypdf import PdfReader
 from pptx import Presentation
 from urllib.parse import urlparse
+from uuid import UUID
+from sqlalchemy.orm import Session
+
+from app.services.document_service import DocumentService
+from app.services.chunking_service import ChunkingService
+from app.repositories.chunk_repository import ChunkRepository
+from app.models.document import DocumentStatus
+from app.services.embedding_service import EmbeddingService
+from app.repositories.document_repository import DocumentRepository
+
 
 def get_page_count(file_bytes: bytes, filename: str) -> int:
     file_stream = io.BytesIO(file_bytes)
@@ -22,6 +32,29 @@ def get_page_count(file_bytes: bytes, filename: str) -> int:
 
     return 0
 
+def process_document_task(db: Session, document_id: UUID, file_bytes: bytes, filename: str):
+    try:
+        # 1. EXTRACT TEXT
+        document_text = DocumentService.extract_text(file_bytes, filename)
+
+        # 2. GENERATE CHUNKS
+        document_chunks = ChunkingService.split_text(document_text)
+
+        # 3. SAVE CHUNKS TO DB
+        chunks = ChunkRepository.bulk_create_chunks(
+            db, document_id, document_chunks
+        )
+
+        EmbeddingService.create_embeddings(db, chunks)
+
+        DocumentRepository.update_document_status(
+            db, document_id, DocumentStatus.ready
+        )
+    except Exception as e:
+        print(f"Error processing document {document_id}: {e}")
+        DocumentRepository.update_document_status(
+            db, document_id, DocumentStatus.failed
+        )
 
 def extract_path_from_supabase_url(file_url: str) -> str:
     parsed = urlparse(file_url)
@@ -35,4 +68,3 @@ def extract_path_from_supabase_url(file_url: str) -> str:
     object_path = "/".join(path_parts[bucket_index + 1 :])
 
     return object_path
-
